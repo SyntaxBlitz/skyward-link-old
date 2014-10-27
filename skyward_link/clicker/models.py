a80700ea75a9475f9a52048c7c61451f06e14fcf
Timothy Aveni <timothy@tacosareawesome.com>
2014-10-27 18:09:12 -0400

    Add ShortLink model
    this will store records of temporary shortened links to match up with the doc link.

--- skyward_link/clicker/models.py
+++ skyward_link/clicker/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class ShortLink(models.Model):
+  short_name = models.CharField(max_length=40)
+  long_link = models.CharField(max_length=256)
