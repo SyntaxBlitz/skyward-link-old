ea80f48c9b301bfcd0bd384ed5edf43a9f50cf75
Timothy Aveni <timothy@tacosareawesome.com>
2014-10-29 21:09:13 -0400

    Add ShortLinkAdmin

--- skyward_link/clicker/admin.py
+++ skyward_link/clicker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from skyward_link.clicker.models import ShortLink
 
-# Register your models here.
+class ShortLinkAdmin(admin.ModelAdmin):
+  pass
+admin.site.register(ShortLink, ShortLinkAdmin)  
